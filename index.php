cda983807f88ae86d13e2c8c7f38767d31228f1e
Alejandro Henkel <alehenkel17@gmail.com>
2016-03-05 19:33:22 -0600

    second try to set heroku

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 	<h1>Hello World!</h1>
+	<p>First try</p>
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
