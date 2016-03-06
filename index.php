1338e930942adcabb09da990a47146b23838a9cd
Alejandro Henkel <alehenkel17@gmail.com>
2016-03-06 14:06:18 -0600

    New controls for slider

--- index.php
+++ index.php
@@ -84,13 +84,83 @@
 			    	</nav>
 				</section>
 			</header>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+					<li data-target="#myCarousel" data-slide-to="3"></li>
+				</ol>
+				<div class="carousel-inner" role="listbox">
+					<div class="item active carousel-img" style="background-image: url('images/rectoria.jpg');">
+						<div id="overlay">
+							<div class="container">
+								<div class="carousel-caption">
+									<h1>Example headline.</h1>
+									<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+									<p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="item carousel-img" style="background-image: url('images/factorTec.png');">
+						<div id="overlay">
+							<div class="container">
+								<div class="carousel-caption">
+									<h1>Another example headline.</h1>
+									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+									<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="item carousel-img" style="background-image: url('images/lab.jpg');">
+						<div id="overlay">
+							<div class="container">
+								<div class="carousel-caption">
+									<h1>One more for good measure.</h1>
+									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+									<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="item carousel-img" style="background-image: url('images/sentidoHumano.png');">
+						<div id="overlay">
+							<div class="container">
+								<div class="carousel-caption">
+									<h1>One more for good measure.</h1>
+									<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+									<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a> -->
+				<a class="triangle-next" href="#myCarousel" role="button" data-slide="next">
+					<div class="triangle-1"></div>
+					<div class="triangle-2"></div>
+				</a>
+				<a class="triangle-prev" href="#myCarousel" role="button" data-slide="prev">
+					<div class="triangle-3"></div>
+					<div class="triangle-4"></div>	
+				</a>
+		    </div>
 		</div>
 		<!-- /page -->
 		<div class="page" id="page-docu">
 			
 		</div>
 		<div class="page" id="page-manuals">
-			
 		</div>
 		<div class="page" id="page-software">
 			
